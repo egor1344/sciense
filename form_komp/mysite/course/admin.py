5555aa418b12dc15bbbcc3085224bcb79385b60a
Egor <gjyzdby@yandex.ru>
2016-05-01 16:54:00 +0500

    Сделал модель и админку. Доделать админку

--- form_komp/mysite/course/admin.py
+++ form_komp/mysite/course/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Course, PredCourse, Competence
 
-# Register your models here.
+
+class PredCourseLine(admin.StackedInline):
+    model = PredCourse
+    filter_horizontal = ('prcours',)
+
+    def get_extra(self, request, obj=None, **kwargs):
+        extra = 1
+        if obj:
+            return extra
+        return extra
+
+
+class CompetenceLine(admin.StackedInline):
+    model = Competence
+
+    # def get_extra(self, request, obj=None, **kwargs):
+    #     extra = 1
+    #     if obj:
+    #         return extra
+    #     return extra
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'tr']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [
+        PredCourseLine,
+    ]
+admin.site.register(Course, CourseAdmin)
+
+
+class CompetenceAdmin(admin.ModelAdmin):
+    list_display = ('cod', 'about', 'tr')
+    filter_horizontal = ('course',)
+admin.site.register(Competence, CompetenceAdmin)
+
+
+class PredCourseAdmin(admin.ModelAdmin):
+    list_display = ['cours']
+    filter_horizontal = ('prcours',)
+admin.site.register(PredCourse, PredCourseAdmin)
--- form_komp/mysite/course/models.py
+++ form_komp/mysite/course/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from university.models import TrainingPrograms
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    about = models.TextField(blank=True)
+    tr = models.ForeignKey(TrainingPrograms)
+    slug = models.SlugField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class PredCourse(models.Model):
+    cours = models.ForeignKey('Course',
+                              related_name='pr_cours')
+    prcours = models.ManyToManyField('Course',
+                                     related_name='pr_predcours',
+                                     blank=True)
+
+
+class Competence(models.Model):
+    cod = models.CharField(max_length=6)
+    about = models.TextField()
+    tr = models.ForeignKey(TrainingPrograms)
+    course = models.ManyToManyField('Course')
+
+    def __str__(self):
+        return self.cod
