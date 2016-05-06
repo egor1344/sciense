465f7c7f9494c41dc418051a3cefc0ce89cb951d
Egor <gjyzdby@yandex.ru>
2016-05-06 21:18:52 +0500

    Доработал вывод предшествующих дисциплин и компетенций

--- form_komp/mysite/course/views.py
+++ form_komp/mysite/course/views.py
@@ -21,11 +21,15 @@ def course_detail(request, slug, slug_c):
     try:
         pred_list = pred_list[0].prcours.all()
     except ObjectDoesNotExist:
-            pred_list = []
-    except  IndexError:
-            pred_list =[]
+        pred_list = []
+    except IndexError:
+        pred_list = []
+    predcomp_list = {}
+    for pred in pred_list:
+        predcomp_list[pred] = Competence.objects.filter(tr__slug=slug)\
+            .filter(course__slug=pred.slug)
     return render(request,
                   "course/list/course_detail.html",
                   {'comp_list': comp_list,
-                   'pred_list': pred_list,
-                   'course': course})
+                   'course': course,
+                   'predcomp_list': predcomp_list})
