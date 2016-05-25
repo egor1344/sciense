5c953c356a190484d0c466fef09ab01152236055
Egor <gjyzdby@yandex.ru>
2016-05-25 19:29:02 +0500

    Сделал вывод в xlsx

--- form_komp/mysite/course/urls.py
+++ form_komp/mysite/course/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<slug>[-\w]+)/$', views.course_list, name = 'course_list'),
-    url(r'^(?P<slug>[-\w]+)/(?P<slug_c>[-\w]+)/$', views.course_detail, name='course_detail'),
+    url(r'^(?P<slug>[-\w]+)/$', views.course_list, name='course_list'),
+    url(r'^(?P<slug>[-\w]+)/(?P<slug_c>[-\w]+)/$',
+        views.course_detail,
+        name='course_detail'),
+    url(r'^(?P<slug>[-\w]+)/(?P<slug_c>[-\w]+)/xlsx/$',
+        views.course_xlsx,
+        name='course_xlsx'),
 ]
--- form_komp/mysite/course/views.py
+++ form_komp/mysite/course/views.py
@@ -1,7 +1,26 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from .models import Course, PredCourse, Competence
 from django.core.exceptions import ObjectDoesNotExist
+from openpyxl import Workbook
+from django.http import HttpResponse
 
+
+def form_komp(course=None):
+    list_comp = Competence.objects.filter(tr=course.tr)\
+                                  .filter(course=course)
+    pred_list = PredCourse.objects.all().filter(cours=course)
+    try:
+        pred_list = pred_list[0].prcours.all()
+    except ObjectDoesNotExist:
+        pred_list = []
+    except IndexError:
+        pred_list = []
+    predcomp_list = {}
+    for pred in pred_list:
+        predcomp_list[pred] = Competence.objects.filter(tr=course.tr)\
+                                                .filter(course__slug=pred.slug)
+    return list_comp, predcomp_list
 
 def course_list(request, slug):
     c_list = Course.objects.filter(tr__slug=slug)
@@ -15,21 +34,40 @@ def course_detail(request, slug, slug_c):
         Всю эту хуйню переделать
     """
     course = Course.objects.get(slug=slug_c)
-    comp_list = Competence.objects.filter(tr__slug=slug)\
-                                  .filter(course__slug=slug_c)
-    pred_list = PredCourse.objects.all().filter(cours__slug=slug_c)
-    try:
-        pred_list = pred_list[0].prcours.all()
-    except ObjectDoesNotExist:
-        pred_list = []
-    except IndexError:
-        pred_list = []
-    predcomp_list = {}
-    for pred in pred_list:
-        predcomp_list[pred] = Competence.objects.filter(tr__slug=slug)\
-            .filter(course__slug=pred.slug)
+    comp_list, predcomp_list = form_komp(course)
     return render(request,
                   "course/list/course_detail.html",
                   {'comp_list': comp_list,
                    'course': course,
                    'predcomp_list': predcomp_list})
+
+def course_xlsx(request, slug, slug_c):
+    response = HttpResponse(content_type='application/vnd.ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="Course.xlsx"'
+
+    course = Course.objects.get(slug=slug_c)
+    list_comp, predcomp_list = form_komp(course)
+    wb = Workbook()
+    ws = wb.active
+    ws.append(['Название дисциплины', course.name ])
+    ws.append([])
+    if predcomp_list:
+        ws.append(['Входящие компетенции'])
+        ws.append(["Код","Описание","Предшествующая дисциплина"])
+        for pred, comp_list in predcomp_list.items():
+            for comp in comp_list:
+                ws.append([comp.cod,comp.about,pred.name])
+    else:
+        ws.append(['Входящих компетенций нет'])
+    ws.append([])
+
+    if list_comp:
+        ws.append(['Исходящие компетенции'])
+        ws.append(["Код","Описание"])
+        for comp in list_comp:
+            ws.append([comp.cod, comp.about])
+    else:
+        ws.append(['Исходящих компетенций нет'])
+
+    wb.save(response)
+    return response
